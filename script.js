6188edbcac211227860e854509e74259d1af2adb
BlackBunnyy <2549intanon@gmail.com>
2024-08-10 21:20:15 +0700

    Update and rename script.js to ไผต่อ3.html

--- script.js
+++
@@ -1,5 +0,0 @@
-// คุณสามารถเพิ่มฟังก์ชัน JavaScript ที่ต้องการเพื่อเพิ่มฟีเจอร์ต่างๆ ได้
-document.addEventListener("DOMContentLoaded", function() {
-    // ตัวอย่าง: สามารถใช้ JavaScript เพื่อสร้างแอนิเมชั่นหรือลูกเล่นเพิ่มเติม
-    console.log("Page loaded successfully.");
-});
